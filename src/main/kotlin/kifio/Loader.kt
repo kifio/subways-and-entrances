e77670cd77b2852027da28ff2738ed100dba25e7
kifio <kg.of.morphine@gmail.com>
2018-10-12 01:59:41 +0300

    Uploading subways and entrances to Firebase

--- src/main/kotlin/kifio/Loader.kt
+++ src/main/kotlin/kifio/Loader.kt
@@ -1,14 +1,14 @@
 package kifio
 
 import java.io.*
+import java.net.*
 import java.lang.*
 import java.util.*
+import com.google.gson.*
 import com.google.api.client.googleapis.auth.oauth2.*
 
-fun main(args: Array<String>) {
-	loadFromTextFile("stations.csv")
-	println(generateToken())
-}
+val baseUrl = "https://subways-and-entrances.firebaseio.com/"
+val token = generateToken()
 
 private fun generateToken(): String {
 	val serviceAccount = FileInputStream("pkey.json")
@@ -20,27 +20,91 @@ private fun generateToken(): String {
 	return scoped.getAccessToken()
 }
 
-private fun loadFromTextFile(path: String) {
-	val stations = mutableListOf<String>()
-    File(path).useLines { lines -> stations.addAll(lines) }
-    stations.forEachIndexed { i, line -> println("$i: ${buildStation(line)}") }
+fun main(args: Array<String>) {
+	loadFromTextFile("stations", ::buildStation)
+	loadFromTextFile("entrances", ::buildEntrance)
 }
 
-private fun buildStation(stationData: String): Station {
-	val attrs = stationData.split(",")
+private fun <T> loadFromTextFile(type: String, initializer: (attrs: List<String>) -> T) {
+	val path = "$type.csv"
+	val url = URL("$baseUrl/$type.json?access_token=$token")
+	val objects = mutableListOf<String>()
+	File(path).useLines { objects.addAll(it) }
+
+	try {
+		objects.forEach { sendJsonToFirebase(Gson().toJson(initializer(it.split(","))), url) }
+    } catch (e: IOException) {
+    	e.printStackTrace()
+    }
+}
+
+private fun buildStation(attrs: List<String>): Station {
+	val geoPoint = parseGeoPoint(attrs)
+	var name: String? = null
+	var color: String? = null
+
+	attrs.map { it.split(" => ") }.forEach {
+		if (it.size == 2) {
+			val key = it[0].replace("\"", "").trim()
+			val value = it[1].replace("\"", "")
+			if (key == "name") {
+				name = value
+			} else if (key == "colour") {
+				color = value
+			}
+		}
+	}
+
+	return Station(UUID.randomUUID().toString(), name, color, geoPoint.first, geoPoint.second)
+}
+
+private fun buildEntrance(attrs: List<String>): Entrance {
+	val geoPoint = parseGeoPoint(attrs)
+	var ref: String? = null
+	var color: String? = null
+
+	attrs.map { it.split(" => ") }.forEach {
+		if (it.size == 2) {
+			val key = it[0].replace("\"", "").trim()
+			val value = it[1].replace("\"", "")
+			if (key == "ref") {
+				ref = value
+			} else if (key == "colour") {
+				color = value
+			}
+		}
+	}
+
+	return Entrance(UUID.randomUUID().toString(), ref?.toInt() ?: 0, color, geoPoint.first, geoPoint.second)
+}
+
+private fun parseTags(entries: List<String>, tags: List<String>): HashMap<String, String> {
+	//TOOD: Create HashMap. Fill it with entries. Search in map by keys,
+}
+
+private fun parseGeoPoint(attrs: List<String>): Pair<Double, Double> {
 	if (attrs.size < 2) throw IllegalArgumentException("Station data must contains tags, lon and lat")
-	var lat: Double = attrs[attrs.size - 1].toDouble()
-	var lon: Double = attrs[attrs.size - 2].toDouble()
-	var name: String = findName(attrs)
-	return Station(UUID.randomUUID().toString(), name, lat, lon)
+	return(Pair<Double, Double>(attrs[attrs.size - 1].toDouble(), attrs[attrs.size - 2].toDouble()))
 }
 
-private fun findName(tags: List<String>): String {
-	val tags: List<String> =  tags.map { tag -> tag.split(" => ") }
-		.filter { pair -> pair[0].trim() == "name" }
-		.map { pair -> pair[1] }
-	if (tags.size != 1) throw IllegalArgumentException("Multiple names is not allowed!")
-	return tags[0].replace("\"", "")
-} 
+private data class Station(val id: String, val name: String?, val color: String?, val lat: Double, val lon: Double)
+private data class Entrance(val id: String, val ref: Int, val color: String?, val lat: Double, val lon: Double)
 
-private data class Station(val id: String, val name: String, val lat: Double, val lon: Double)
+@Throws(IOException::class)
+private fun sendJsonToFirebase(jsonString: String, url: URL)  {
+	val connection = url.openConnection() as HttpURLConnection
+	connection.setRequestMethod("POST")
+	connection.setDoOutput(true);
+
+	val os = OutputStreamWriter(connection.getOutputStream());
+	os.write(jsonString);
+	os.flush();
+	os.close();
+
+	val responseCode = connection.getResponseCode();
+	println("Sending $jsonString to URL : " + url);
+	println("Response Code : " + responseCode);
+	
+	val response = connection.getInputStream().bufferedReader().use(BufferedReader::readText)
+	println(response.toString());
+}
