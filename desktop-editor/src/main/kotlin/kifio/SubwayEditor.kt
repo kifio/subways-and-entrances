0eb2f40a2501a88a6d1db2ad2e6d18802e3720d6
kifio <kg.of.morphine@gmail.com>
2019-01-17 18:12:08 +0300

    Add tornadofx to desktop subways editor

---
+++ desktop-editor/src/main/kotlin/kifio/SubwayEditor.kt
@@ -0,0 +1,8 @@
+package kifio
+
+import javafx.application.Application
+import tornadofx.App
+
+class SubwayEditor: App(RootView::class) {
+
+}
--- loader/src/main/kotlin/kifio/Loader.kt
+++ loader/src/main/kotlin/kifio/Loader.kt
@@ -1,12 +1,11 @@
 package kifio
 
-import com.mapbox.geojson.FeatureCollection
 import kifio.MetroMap.buildMap
+import kifio.subway.data.Entrance
 import kifio.subway.data.Station
-import java.io.File
 import java.io.FileInputStream
 
-fun main(args: Array<String>) {
+fun readMetroMap(args: Array<String>): Map<Station, List<Entrance>> {
 
     val efis: FileInputStream
     val sfis: FileInputStream
@@ -27,18 +26,17 @@ fun main(args: Array<String>) {
                 "or use default version of files")
     }
 
-    writeMapToFile(buildMap(efis, sfis), outputFile)
+    return buildMap(efis, sfis)
 }
 
-private fun writeMapToFile(map: Map<Station, FeatureCollection>, outputFile: String) {
-    val sb = StringBuilder()
-    File(outputFile).bufferedWriter().use { out ->
-        for (station in map.keys) {
-            val entrances = map[station]?.features() ?: throw java.lang.IllegalArgumentException("Features list cannot be empty")
-            for (entrance in entrances) {
-//                TODO: Save json
-//                entrance.properties()
-            }
-        }
-    }
-}
+//private fun writeMapToFile(map: Map<Station, FeatureCollection>, outputFile: String) {
+//    val sb = StringBuilder()
+//    File(outputFile).bufferedWriter().use { out ->
+//        for (station in map.keys) {
+//            val entrances = map[station]?.features() ?: throw java.lang.IllegalArgumentException("Features list cannot be empty")
+//            for (entrance in entrances) {
+//
+//            }
+//        }
+//    }
+//}
